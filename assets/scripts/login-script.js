2aa9dcd89d7554568920c67e588e2f1ce5af0587
zan-cloud <136007145+zan-cloud@users.noreply.github.com>
2025-01-17 10:58:11 +0700

    Update login-script.js
    mengganti komentar sesuai dengan JSDocs

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,33 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+/**
+ * Menangani pengiriman formulir login dan memeriksa kecocokan email dan kata sandi.
+ * Jika email dan kata sandi cocok, mengarahkan ke halaman utama.
+ * Jika tidak cocok, menampilkan pop-up error.
+ * 
+ * @event submit
+ * @param {Event} event - Peristiwa pengiriman formulir login.
  */
-
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+// Email dan kata sandi yang diharapkan untuk login.
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/**
+ * Menangani pengiriman formulir login.
+ * @param {Event} event - Peristiwa pengiriman formulir yang memicu fungsi ini.
+ */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+  event.preventDefault(); // Mencegah pengiriman formulir yang sebenarnya
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
+  const email = inputEmailElement.value; // Mengambil nilai email dari input
+  const password = inputPasswordElement.value; // Mengambil nilai kata sandi dari input
+
+  // Memeriksa apakah email dan kata sandi cocok dengan yang diharapkan
   if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
+    goToHome(); // Mengarahkan ke halaman utama jika login berhasil
   } else {
-    showPopUp();
+    showPopUp(); // Menampilkan pop-up error jika login gagal
   }
 });
